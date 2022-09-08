4f4b4ede9da3b0704dd0c6a4847bee614b65f075
Videet Malpe <videet@MacBook-Pro.lan>
2022-09-08 19:54:02 -0400

    Played with materizations like view,table,ephemeral and incremental

---
+++ models/dim/dim_hosts_clean.sql
@@ -0,0 +1,13 @@
+
+
+WITH SRC_HOSTS
+AS (
+	SELECT *
+	FROM {{ref('src_hosts') }}
+	)
+SELECT host_id,
+	NVL(HOST_NAME,'Anonymous') as Host_name,
+	is_superhost,
+	created_at,
+	updated_at
+FROM SRC_HOSTS
